13845001971c4106a5759425153aeda1a767ec7c
AnishS109 <anishsaini9098@gmail.com>
2024-10-02 15:02:22 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,14 @@
+a = {"anish":"sani",
+     "marks": 100,
+      "list":[1,4,6]
+}
+
+# print(a.items())
+
+# print (a.keys())
+
+# print (a.values())
+
+# a.update({"anish":"Saini"})
+# print(a["anish"])
+
